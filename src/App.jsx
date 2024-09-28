39239ce1ac47a59c125a80fb8845162ade8dfd00
Olena Shatalova <08082023m@gmail.com>
2024-09-28 15:10:17 +0300

    'initial commit'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Comp1 from './copmonents/Comp1/Comp1';
+import Comp2 from './copmonents/Comp2/Comp2';
+
+function App() {
+  return (
+    <>
+      <Comp1 />
+      <Comp2 />
+    </>
+  );
+}
+
+export default App;
